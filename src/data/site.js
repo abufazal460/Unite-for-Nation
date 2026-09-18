f5a8d2854772a786f621becc048cd54b9f24ee5e
Abu Fazal <abufazal460@gmail.com>
2026-09-18 12:47:09 +0530

    remove useless logo images in assets and import path remove in site.js file

--- src/data/site.js
+++ src/data/site.js
@@ -1,11 +1,9 @@
-// import logo from "../assets/logo/logo.webp"
-// import logo2 from "../assets/logo/logo.png"
-import logo3 from "../assets/logo/logo-removebg.png"
+import logo from "../assets/logo/brand-logo.png"
 export const site = {
   name: "Unite For Nation",
   fullName: "Unite For Nation Human Rights Foundation",
   tagline: "Defending Innocents & Restoring Human Rights",
-  logoUrl: logo3,
+  logoUrl: logo,
   whatsappNumber: "919452900007", // International WhatsApp format without plus
   whatsappMessage: "Hello%20Unite%20For%20Nation,%20I%20need%20urgent%20legal%20guidance%20regarding%20a%20false%20case.",
   whatsappUrl: "https://wa.me/919452900007?text=Hello%20Unite%20For%20Nation,%20I%20need%20legal%20guidance.",
