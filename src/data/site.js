6d69397f783919a6f2922a10fb99c9b830c80a1a
Abu Fazal <abufazal460@gmail.com>
2026-09-18 12:37:17 +0530

    whatsapp msg spelling fixed unit of nation to unite for nation

--- src/data/site.js
+++ src/data/site.js
@@ -1,5 +1,5 @@
-import logo from "../assets/logo/logo.webp"
-import logo2 from "../assets/logo/logo.png"
+// import logo from "../assets/logo/logo.webp"
+// import logo2 from "../assets/logo/logo.png"
 import logo3 from "../assets/logo/logo-removebg.png"
 export const site = {
   name: "Unite For Nation",
@@ -7,8 +7,8 @@ export const site = {
   tagline: "Defending Innocents & Restoring Human Rights",
   logoUrl: logo3,
   whatsappNumber: "919452900007", // International WhatsApp format without plus
-  whatsappMessage: "Hello%20Unite%20of%20Nation,%20I%20need%20urgent%20legal%20guidance%20regarding%20a%20false%20case.",
-  whatsappUrl: "https://wa.me/919452900007?text=Hello%20Unite%20of%20Nation,%20I%20need%20legal%20guidance.",
+  whatsappMessage: "Hello%20Unite%20For%20Nation,%20I%20need%20urgent%20legal%20guidance%20regarding%20a%20false%20case.",
+  whatsappUrl: "https://wa.me/919452900007?text=Hello%20Unite%20For%20Nation,%20I%20need%20legal%20guidance.",
   email: "unitefornationorg@gmail.com",
   address: "A-29 Batla House Chowk Jamia Nagar Okhla Delhi-25",
   registrationNo: "UP/2022/0318038",
